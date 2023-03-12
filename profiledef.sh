048f1a7a1be116442a8324e9efb173b3c74f044d
Mauricio de Lima <mauriciodelima.mol@gmail.com>
2023-03-11 23:02:07 -0300

    Ajustes

--- profiledef.sh
+++ profiledef.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="uaisocli"
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="UaiSO Linux <https://uaiso.uai21.online>"
-iso_application="UaiSO Linux Live/Rescue CD"
-iso_version="$(date +%Y-%m-%d-%H)"
+iso_name="archlinux"
+iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="Arch Linux <https://archlinux.org>"
+iso_application="Arch Linux Live/Rescue CD"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
@@ -14,8 +14,6 @@ bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-# airootfs_image_tool_options=('-Xcompression-level' '1' '-b' '1M')
-# airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '15' '-b' '1M')
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
@@ -24,9 +22,4 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/etc/polkit-1/rules.d"]="0:0:750"
-  ["/etc/sudoers.d"]="0:0:750"
-  ["/usr/local/bin/alci-make-a-pure-arch"]="0:0:755"
-  ["/usr/local/bin/alci-displaymanager-check"]="0:0:755"
-  ["/usr/local/bin/get-nemesis-on-alci"]="0:0:755"
 )
